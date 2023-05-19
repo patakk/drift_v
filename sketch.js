4562f413e92db7c66fbcc7eb80803d84afeaf968
patakk <paolo.ceric@gmail.com>
2023-05-19 03:10:22 +0200

    good

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-import { rand, power, map, colorLightness } from './utils/utils.js';
+import { rand, power, map, colorLightness, rgb2hsv, hsv2rgb } from './utils/utils.js';
 import { noiseSeed } from './utils/noise.js';
 import { getShaderSource, createShader, createProgram } from './utils/webglutils.js';
 
@@ -74,66 +74,6 @@ let u_color2;
 let u_tint1;
 let u_tint2;
 
-function rgb2hsv(r, g, b) {
-    let max = Math.max(r, g, b);
-    let min = Math.min(r, g, b);
-    let h, s, v = max;
-  
-    let d = max - min;
-    s = max === 0 ? 0 : d / max;
-  
-    if (max === min) {
-        h = 0; // achromatic
-    } else {
-        switch (max) {
-            case r:
-                h = (g - b) / d + (g < b ? 6 : 0);
-                break;
-            case g:
-                h = (b - r) / d + 2;
-                break;
-            case b:
-                h = (r - g) / d + 4;
-                break;
-        }
-        h /= 6;
-    }
-  
-    return [h, s, v];
-}
-
-function hsv2rgb(h, s, v) {
-    let r, g, b;
-  
-    let i = Math.floor(h * 6);
-    let f = h * 6 - i;
-    let p = v * (1 - s);
-    let q = v * (1 - f * s);
-    let t = v * (1 - (1 - f) * s);
-  
-    switch (i % 6) {
-        case 0:
-            r = v, g = t, b = p;
-            break;
-        case 1:
-            r = q, g = v, b = p;
-            break;
-        case 2:
-            r = p, g = v, b = t;
-            break;
-        case 3:
-            r = p, g = q, b = v;
-            break;
-        case 4:
-            r = t, g = p, b = v;
-            break;
-        case 5:
-            r = v, g = p, b = q;
-            break;
-    }
-  
-    return [r, g, b];
-}
 
 function choicesInit(){
     let aspect = {x: 3, y: 4}
--- utils/utils.js
+++ utils/utils.js
@@ -29,3 +29,65 @@ export function map(x, v1, v2, v3, v4){
 export function colorLightness(r, g, b){
     return 0.2126*r + 0.7152*g + 0.0722*b;
 }
+
+
+function rgb2hsv(r, g, b) {
+    let max = Math.max(r, g, b);
+    let min = Math.min(r, g, b);
+    let h, s, v = max;
+  
+    let d = max - min;
+    s = max === 0 ? 0 : d / max;
+  
+    if (max === min) {
+        h = 0; // achromatic
+    } else {
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+        h /= 6;
+    }
+  
+    return [h, s, v];
+}
+
+function hsv2rgb(h, s, v) {
+    let r, g, b;
+  
+    let i = Math.floor(h * 6);
+    let f = h * 6 - i;
+    let p = v * (1 - s);
+    let q = v * (1 - f * s);
+    let t = v * (1 - (1 - f) * s);
+  
+    switch (i % 6) {
+        case 0:
+            r = v, g = t, b = p;
+            break;
+        case 1:
+            r = q, g = v, b = p;
+            break;
+        case 2:
+            r = p, g = v, b = t;
+            break;
+        case 3:
+            r = p, g = q, b = v;
+            break;
+        case 4:
+            r = t, g = p, b = v;
+            break;
+        case 5:
+            r = v, g = p, b = q;
+            break;
+    }
+  
+    return [r, g, b];
+}
